5572058fc6bf9c3f7e2d95036b20ec6990433596
potato865 <724895582@qq.com>
2021-04-05 01:18:03 +0800

    add some type

--- src/app/components/config-form/config-form-model.ts
+++ src/app/components/config-form/config-form-model.ts
@@ -1,22 +1,24 @@
 import { FormArray, FormControl, FormGroup, ValidatorFn } from "@angular/forms";
 export type IControlValue = string | number | null;
+export type IType = 'input' | 'select';
+export type IControlType = 'group' | 'control' | 'array';
 export interface IOption {
   label: string;
   value: any;
 }
 export interface IControlConfig {
-  type: 'input' | 'select';
-  span: number;
-  offset: number;
-  layout: number[];
+  type: IType;
   label: string;
   key: string;
   value: IControlValue;
+  controlType: IControlType;
+  rowIndex: number;
+  span: number;
+  offset: number;
+  layout: number[];
   validators: ValidatorFn | ValidatorFn[];
   showError?: (control: FormControl, config: IControlConfig) => string[];
   onChange?: (value: IControlValue, form: FormGroup, formConfig: IControlConfig[], updateForm:()=>void) => void;
-  rowIndex: number;
-  controlType: 'group' | 'control' | 'array';
   children?: IControlConfig[];
   control?: FormControl | FormGroup | FormArray;
   options?: IOption[];
