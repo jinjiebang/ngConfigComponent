4e11ba9f0a937e1bc77569495483af2f2e579384
potato865 <724895582@qq.com>
2021-04-06 02:33:30 +0800

    showErros pipe change

--- src/app/components/config-form-item/showErrors.pipe.ts
+++ src/app/components/config-form-item/showErrors.pipe.ts
@@ -2,29 +2,39 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { IControlConfig } from '../config-form/config-form-model';
 
+export interface IErrorConfig {
+  label: string;
+  showError?: (control: FormControl, config: IErrorConfig) => string[]
+}
 @Pipe({
   name: 'showErrors',
   pure: true,
 })
-export class ShowErrorsPipe implements PipeTransform {
+export class ShowErrorsPipe<T extends IErrorConfig> implements PipeTransform {
 
-  transform(control: FormControl, config: IControlConfig): any {
+  transform(control: FormControl, config: T | string): string[] {
     const errors = control.errors;
     const dirty = control.dirty;
     if (!errors || !dirty) return [];
-    if (config.showError) return config.showError(control, config);
+    let label:string
+    if (typeof config === 'string') {
+      label = config;
+    } else {
+      label = config.label;
+      if (config.showError) return config.showError(control, config);
+    }
     return Object.keys(errors).map(key => {
       const error = errors[key];
       if (key === 'required') {
-        return `请输入${config.label}`;
+        return `请输入${label}`;
       } else if (key === 'min') {
-        return `${config.label}最小值是${error.min}`
+        return `${label}最小值是${error.min}`
       } else if (key === 'max') {
-        return `${config.label}最大值是${error.max}`
+        return `${label}最大值是${error.max}`
       } else if (key === 'minlength') {
-        return `${config.label}最小长度是${error.requiredLength}`
+        return `${label}最小长度是${error.requiredLength}`
       } else if (key === 'maxlength') {
-        return `${config.label}最大长度是${error.requiredLength}`
+        return `${label}最大长度是${error.requiredLength}`
       } else {
         return 'unkownn error';
       }
